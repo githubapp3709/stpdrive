229fb424cb98a0bf90ea56c4475ae85059ff2f1d
shakti <githubapp3709@gmail.com>
2024-03-28 16:32:58 +0530

    created meunu bar

--- index.php
+++ index.php
@@ -4,6 +4,43 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+   <!-- customized css -->
+   <link rel="stylesheet" href="css/style.css">
+   <!-- bootstrap js -->
+   <script src="js/bootstrap.bundle.min.js"></script>
+  <!-- jquery google library -->
+  <script src="js/jquery.min.js"></script>
   </head>
-  <body></body>
+  <body>
+    <!-- navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="images/logo.png" alt="logo" width="90px">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item px-3">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+        <li class="nav-item px-3">
+          <a class="nav-link" href="#">About us</a>
+        </li>
+        <li class="nav-item px-3 bg-light rounded">
+          <a class="nav-link text-dark" href="#">Login</a>
+        </li>      
+    </ul>  
+    </div>
+  </div>
+</nav>
+
+  </body>
 </html>
